425fa6bd141f06de10363f31cb23f5007a2b9157
Christian F. Baumgartner <c.f.baumgartner@gmail.com>
2016-09-22 14:10:18 +0100

    changed cuda libraries

--- runongpuX.sh
+++ runongpuX.sh
@@ -5,6 +5,6 @@ echo "Running $2 on GPU$1"
 CUDA_VISIBLE_DEVICES=''
 CUDA_VISIBLE_DEVICES=$1; export CUDA_VISIBLE_DEVICES
 
-THEANO_FLAGS=floatX=float32,device=gpu0,force_device=True,base_compile_dir=/homes/cbaumgar/.theano2 PATH=${PATH}:/vol/cuda/7.5.18/bin/ LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/vol/cuda/7.5.18/lib64:/vol/biomedic/users/cbaumgar/software/cuDNN/cuda/lib64 CPATH=/vol/biomedic/users/cbaumgar/software/cuDNN/cuda/include/ LIBRARY_PATH=/vol/biomedic/users/cbaumgar/software/cuDNN/cuda/lib64/ python $2
+THEANO_FLAGS=floatX=float32,device=gpu0,force_device=True PATH=${PATH}:/vol/cuda/7.5.18/bin/ LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/vol/cuda/7.5.18/lib64 CPATH=/vol/cuda/7.5.18/lib64/include LIBRARY_PATH=/vol/cuda/7.5.18/lib64 python $2
 
 
